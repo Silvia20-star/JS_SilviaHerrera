345bba1fa2311b6c4e705be331d3293bb13be76b
Silvia20-star <Silvitahg@comunidad.unam.mx>
2020-06-20 23:59:53 -0500

    FEAT: se agregó Actividad 6. Construyendo con Dom

---
+++ Dynamics/JS/DOM.js
@@ -0,0 +1,9 @@
+let cuerpo= document.getElementsByTagName("body")[0];
+cuerpo.appendChild(div);
+var div = document.createElement("div");
+div.setAttribute=("DIV");
+document.write("<h1><a href='#'>Soy el más grande...</a></h1>");
+document.write("<h2>Curso Web 2020</h2>");
+document.write("<h2><a href='#'>Mediano</a></h2>");
+document.write("<p>Jelou</p>");
+document.write("<h3>Estoy medio chiquito :(</h3>");
